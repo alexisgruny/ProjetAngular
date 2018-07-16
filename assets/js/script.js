4062c48665453d69d411040e035c66b30d808f78
fadikamohamed <fadika.mohamed02@gmail.com>
2018-07-16 12:09:09 +0200

    .

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var articles = angular.module('content', []);
-articles.controller('contentCtrl', function($scope, $http) {
-  $http.get('assets/js/item.json')
-  .then(function(res) {
-    $scope.item = res.data;
-  });
-});
