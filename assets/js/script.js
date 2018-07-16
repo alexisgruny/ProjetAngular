47781e564695d71fd32e5a83409012d9fae08ca1
AlexisG <alexis.gruny60@gmail.com>
2018-07-16 12:07:12 +0200

    ca marche

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var articles = angular.module('content', []);
 articles.controller('contentCtrl', function($scope, $http) {
-  $http.get('assets/js/item.json')
+  $http.get('assets/js/articles.json')
   .then(function(res) {
-    $scope.item = res.data;
+    $scope.articles = res.data;
   });
 });
