e776626da55b14b29f140d02c1843cf85016ea93
AlexisG <alexis.gruny60@gmail.com>
2018-07-16 10:24:48 +0200

    ajout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var articles = angular.module('content', []);
 articles.controller('contentCtrl', function($scope, $http) {
   $http.get('assets/js/item.json')
-  .then(function(response) {
-    $scope.figurines = response.data;
+  .then(function(res) {
+    $scope.item = res.data;
   });
 });
