4498d4eabcbb9884c1caf9d7f55f1eb6b45f2133
AlexisG <alexis.gruny60@gmail.com>
2018-07-17 13:37:28 +0200

    panier correction

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,24 +2,22 @@
 var items = angular.module('content', []);
 //Déclaration des tableaux pour le panier au lancement de la page
 items.run(['$rootScope', function($rootScope) {
-  $rootScope.productBasket = [];
-  $rootScope.priceBasket = [];
-  $rootScope.refBasket = [];
-  $rootScope.descBasket = [];
-  $rootScope.typeBasket = [];
+  $rootScope.basket = [];
+  $rootScope.prixTotal = 0;
+
 }]);
 //Déclaration du controlleur
-items.controller('contentCtrl',['$rootScope', '$scope', '$http', function($rootScope, $scope, $http) {
+items.controller('contentCtrl',['$scope', '$http', function( $scope, $http) {
   //Appel du fichier .json
   $http.get('assets/js/articles.json')
   .then(function(res) {
-    $scope.articles = res.data;
+  $scope.articles = res.data;
+
   });
   $scope.sendArticles = function(index) {
-    $rootScope.productBasket.push($scope.articles[index].product);
-    $rootScope.priceBasket.push($scope.articles[index].price);
-    $rootScope.refBasket.push($scope.articles[index].ref);
-    $rootScope.descBasket.push($scope.articles[index].desc);
-    $rootScope.typeBasket.push($scope.articles[index].type);
+    $scope.basket.push($scope.articles[index]);
+    for(var key in $scope.basket){
+    $scope.prixTotal += $scope.basket[key].price;
+  }
   }
 }]);
